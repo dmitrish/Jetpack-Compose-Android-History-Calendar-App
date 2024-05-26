128c82d7da88c2d860be730950a199a2cffd4c72
dmitrish <dmitrinyc@yahoo.com>
2024-05-26 11:20:52 -0400

    Setting navigation

--- app/src/main/java/com/coroutines/thisdayinhistory/graph/routes.kt
+++ app/src/main/java/com/coroutines/thisdayinhistory/graph/routes.kt
@@ -1,2 +1,18 @@
 package com.coroutines.thisdayinhistory.graph
 
+enum class NavRoutes {
+    IntroRoute,
+    MainRoute
+}
+enum class MainNavOption {
+    HistoryScreen,
+    DetailScreen,
+    LanguagesScreen,
+    ThemeScreen,
+    AboutScreen
+}
+
+enum class IntroNavOption {
+    WelcomeScreen,
+    LanguagesScreen
+}
