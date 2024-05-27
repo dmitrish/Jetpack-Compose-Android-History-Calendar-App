16dc22b1675b03f407a1010860322f3c200849b7
dmitrish <dmitrinyc@yahoo.com>
2024-05-27 11:53:04 -0400

    Setting navigation

---
+++ app/src/main/java/com/coroutines/thisdayinhistory/MainContent.kt
@@ -0,0 +1,14 @@
+package com.coroutines.thisdayinhistory
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.coroutines.thisdayinhistory.graph.AppNavHost
+
+@Composable
+fun MainContent(){
+    val navController = rememberNavController()
+    AppNavHost(
+        navController = navController,
+        isOnboarded = false
+    )
+}
---
+++ app/src/main/java/com/coroutines/thisdayinhistory/ui/screens/about/AboutScreen.kt
@@ -0,0 +1,13 @@
+package com.coroutines.thisdayinhistory.ui.screens.about
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AboutScreen(
+    modifier: Modifier = Modifier
+){
+    //todo content
+    Text("This is About Screen")
+}
